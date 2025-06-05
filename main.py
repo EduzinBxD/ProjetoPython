74eebb78d1fc7c99f4b9dfbc4050d99b9da360ab
ZzythHer <eduardonrdeoliveira@gmail.com>
2025-06-05 13:27:50 -0300

    Update main.py
    Feito o sistema básico do quiz com apenas uma pergunta por enquanto

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+import time, os, random
+
+print("Bem vindo! Deseja começar o quiz?\nTema: DC")
+
+resposta1 = input("Digite (Sim ou Não): ")
+
+if resposta1 == "Sim":
+    print("O quiz vai começar!")
+else:
+    os.system(exit)
+
+#Pergunta 1:
+print("Primeira Pergunta: Qual o nome do Batman?\n(A) Bruce Williams\n(B) Bruce Banner\n(C) Bruce Wayne\n(D) Clark Kent")
+resposta2 = input("Digite sua resposta: ")
+
+if resposta2 == "C":
+    print("Resposta correta!")
+else:
+    print("Resposta incorreta!")
