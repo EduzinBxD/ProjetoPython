a6679f13da55c3fa2a4f4c85f7484f480f4e3994
guilherme <guiml310507@gmail.com>
2025-06-05 14:50:38 -0300

    Update main.py
    de inicio coloquei apenas 10 perguntas

--- main.py
+++ main.py
@@ -17,3 +17,75 @@ if resposta2 == "C":
     print("Resposta correta!")
 else:
     print("Resposta incorreta!")
+
+#pergunta 2
+print("Qual vilão é conhecido como o 'Príncipe Palhaço do Crime'\n(A)Lex Luthor\n(B)Coringa\n(C)Pinguim\n(D)Exterminador")
+resposta3 = input("Digite sua resposta: ")
+if resposta2 == "B":
+    print("Resposta correta!")
+else:
+    print("Resposta incorreta!")
+    
+    #pergunta 3
+print("De qual planeta a Mulher-Maravilha é originária?\n(A)Krypton\n(B)Themyscira\n(C)Oa\n(D)Apokolips")
+resposta3 = input("digite sua resposta")
+if resposta2 == "B":
+    print("Resposta correta!")
+else:
+    print("Resposta incorreta!")
+
+#pergunta 4
+print("Em qual história o Superman morre lutando contra o Monstro Doomsday?\n(A)Crise nas Infinitas Terras\n(B)A Morte do Superman\n(C)Batman: Cavaleiro das Trevas\n(D)Injustiça: Deuses Entre Nos")
+resposta4 = input("digite sua resposta")
+if resposta2 == "B":
+    print("Resposta correta!")
+else:
+    print("Resposta incorreta!")
+
+    #pergunta 5
+print("Qual arqueiro se tornou líder da Liga da Justiça em algumas versões?\n(A)Arqueiro Verde (Oliver Queen)\n(B) Arqueiro Vermelho (Roy Harper)\n(C)Gavião Negro (Carter Hall)\n(D)Esquadrão Suicida")
+resposta5 = input("digite sua resposta")
+if resposta5 == "A" or "a":
+    print("Resposta correta!")
+else:
+    print("Resposta incorreta!")
+
+    #pergunta 6
+    print("Quem interpretou o Batman nos filmes de Christopher Nolan?\n(A)Ben Affleck\n(B)Christian Bale\n(C)Michael Keaton\n(D)Robert Pattinson")
+    resposta6 = input("digite sua resposta")
+    if resposta6 == "B" or "b"
+    print("resposta correta!")
+else:
+print("resposta incorreta!")
+
+#pergunta 7
+print("Quem é o Flash mais rápido de todos os tempos?\n(A)Barry Allen\n(B)Wally West\n(C)Jay Garrick\n(D)Savitar")
+ resposta7 = input("digite sua resposta")
+    if resposta7 == "B" or "b":
+    print("resposta correta!")
+else:
+print("resposta incorreta!")
+
+#pergunta 8
+print("Qual vilão é conhecido por roubar a velocidade dos outros?\n(A)Zoom (Hunter Zolomon)\n(B)Capitão Frio\n(C)Flash Reverso(D)Gorila Grodd")
+resposta8 = input("digite sua resposta")
+if resposta8 == "A" or "a":
+    print("resposta correta!")
+else:
+print("resposta incorreta!")
+
+#pergunta 9
+print("Qual é a principal fraqueza do Superman?\n(A)Magia\n(B)Kryptonita(C)Chumbo\n(D)Água")
+resposta6 = input("digite sua resposta")
+    if resposta9 == "B" or "b":
+    print("resposta correta!")
+else:
+print("resposta incorreta!")
+
+#pergunta 10
+print("Qual desses personagens NÃO é kryptoniano?\n(A)Supergirl\n(B)General Zod\n(C)Power Girl\n(D)Martian Manhunter")
+resposta6 = input("digite sua resposta")
+if resposta10 == "D" or "d":
+    print("Resposta correta!")
+else:
+    print("Resposta incorreta!")
